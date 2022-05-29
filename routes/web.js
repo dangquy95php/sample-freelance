89e87c7d26f5d6ac47ca69edc834b7e779d64562
dangquy <pxwebshop@gmail.com>
2022-05-29 12:03:17 +0700

    add router

--- routes/web.js
+++ routes/web.js
@@ -3,7 +3,7 @@ const router   = express.Router();
 const LoginController = require('../app/controllers/LoginController');
 
 router.get('/', LoginController.index);
-router.post('/translate', LoginController.postIndex);
+router.post('/login', LoginController.postIndex);
 
 
 module.exports = router;
